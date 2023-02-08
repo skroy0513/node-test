375cdacd129fe0e40b5db21c6bbd2a0bc37636fe
skroy0513 <skroy0513@gmail.com>
2023-02-08 19:05:36 +0900

    code clean up

--- controllers/searchPwd.js
+++ controllers/searchPwd.js
@@ -35,20 +35,4 @@ const postSearchPwdHandle = async (req, res)=>{
   }
 }
 
-// const postSearchPwdHandle =  (req, res)=>{
-//   // 비밀번호 찾기 : 아이디와 이름을 기준으로 찾기
-//   const { userName, userId } = req.body;
-//   console.log(userName, userId);
-
-//   const user = users.find( user => user.userName === userName && user.userId === userId)
-//   let resData = {}
-//   if(user){
-//     resData = { success : true, message : user}
-//   }else{
-//     resData = { success : false, message : "아이디 또는 비밀번호를 확인하세요."}
-//   }
-
-//   res.send(resData);
-// }
-
 module.exports = {getSearchPwdHandle, postSearchPwdHandle};
